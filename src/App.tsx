eee5300e70f32a631de01fbcb6dd7fa541fb5881
NickTremblay <nickt1@uri.edu>
2023-03-02 21:59:36 -0500

    Add UpdateRoomates menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import { Button } from '@mui/material';
+import React, { useState } from 'react';
+import AddRoomateModal from './components/UpdateRoomatesModal';
+import GetSavedRoomates from './util/GetSavedRoomates';
 
 function App() {
+  const [roomates, setRoomates] = useState(GetSavedRoomates() || []);
+  const [isAddRoomateDialogueOpen, setIsAddRoomateDialogueOpen] = useState(false);
+
+  const handleAddRoomateButtonClick = () => { 
+    setIsAddRoomateDialogueOpen(true);
+  }
+
   return (
     <div className="App">
+      <Button onClick={handleAddRoomateButtonClick}>Add Roomate</Button>
+
+      <AddRoomateModal
+        roomates={roomates}
+        setRoomates={setRoomates}
+        isOpen={isAddRoomateDialogueOpen}
+        setIsOpen={setIsAddRoomateDialogueOpen}
+      />
+
     </div>
   );
 }
---
+++ src/components/UpdateRoomatesModal.tsx
@@ -0,0 +1,114 @@
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, List, ListItem, ListItemText, TextField, IconButton } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import React, { useState } from "react";
+import Roomate from "../types/Roomate";
+import SaveRoomates from "../util/SaveRoomates";
+
+interface Props { 
+    roomates:Roomate[]; 
+    setRoomates: (roomates: Roomate[]) => void; 
+    isOpen: boolean;
+    setIsOpen: (isOpen: boolean) => void; 
+}
+
+const AddRoomateModal = (props: Props) => {
+    const {isOpen, setIsOpen, roomates, setRoomates} = props;
+    const [updatedRoomates, setUpdatedRoomates] = useState(roomates);
+    const [nameInputValue, setNameInputValue] = useState("");
+    const [isNameInputValueValid, setIsNameInputValueValid] = useState(true);
+
+    const addRoomate = (name: string) => { 
+        const newRoomate = {
+            name,
+            balance: 0
+        } as Roomate;
+        setUpdatedRoomates((oldUpdatedRoomates) => [...oldUpdatedRoomates, newRoomate]);
+    }
+
+    const deleteRoomate = (name: string) => { 
+        setUpdatedRoomates((oldUpdatedRoomates) => {
+            const newUpdatedRoomates = oldUpdatedRoomates.filter((roomate:Roomate) => roomate.name !== name);
+            return newUpdatedRoomates;
+        });
+    }
+
+    const handleNameInputChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        const newName = event.target.value;
+
+        setNameInputValue(newName);
+
+        if(updatedRoomates.filter((roomate) => roomate.name === newName).length > 0){ 
+            setIsNameInputValueValid(false)
+        }else if(!isNameInputValueValid) setIsNameInputValueValid(true);
+    }
+
+    const handleCloseClick = () => {
+        setIsOpen(false);
+        if(isNameInputValueValid && nameInputValue !== ""){ 
+            addRoomate(nameInputValue);
+            setRoomates(updatedRoomates);
+            SaveRoomates(updatedRoomates);
+            setNameInputValue("");
+            setIsNameInputValueValid(true);
+        }
+    }
+
+    const handleAddNewRoomateClick = () => { 
+        if(isNameInputValueValid && nameInputValue !== ""){ 
+            addRoomate(nameInputValue);
+            setIsNameInputValueValid(true);
+            setNameInputValue("");
+        }
+    }
+
+    return (
+        <Dialog
+            open={isOpen}
+            onClose={handleCloseClick}
+            aria-labelledby="alert-dialog-title"
+            >
+
+            <DialogTitle id="alert-dialog-title">
+                {"Update Roomates"}
+            </DialogTitle>
+
+            <DialogContent>
+                {/* Display each roomate already added */}
+                {updatedRoomates.length > 0 && <List>
+                    {
+                        updatedRoomates.map((roomate:Roomate, i) => { 
+                            return (
+                                <ListItem key={i} disablePadding>
+                                    <ListItemText primary={roomate.name} />
+                                    <IconButton aria-label="delete" onClick={() => deleteRoomate(roomate.name)}>
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </ListItem>
+                            )
+                        })
+                    }
+                </List>}
+
+                <TextField
+                    error={!isNameInputValueValid}
+                    label="Name of new roomate"
+                    helperText={!isNameInputValueValid ? "Name already in use" : null}
+                    onChange={handleNameInputChange}
+                    value={nameInputValue}
+                />
+                
+            </DialogContent>
+
+            <DialogActions>
+                <Button onClick={handleCloseClick}>Done</Button>
+                <Button onClick={handleAddNewRoomateClick} autoFocus>
+                Add Another Roomate
+                </Button>
+            </DialogActions>
+
+        </Dialog>
+    );
+
+}
+
+export default AddRoomateModal;
